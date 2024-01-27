22bf3a1f20c732ff77208e94580528c67d9e5aa8
Louise Dungca <dungca.louisechristine@gmail.com>
2024-01-27 09:22:31 +0800

    fix: add new method for finding smallest int

--- oop.rb
+++ oop.rb
@@ -3,6 +3,8 @@
 arr = [25, 8, -8, 1]
 
 def find_smallest_int(arr)
+  return nil if arr.empty?
+
   ref_int = arr[0]
   arr.each do |int|
     ref_int = int if int < ref_int
@@ -11,6 +13,35 @@ def find_smallest_int(arr)
 end
 
 p find_smallest_int(arr)
+
+## or:
+
+def find_lowest_integer(arr)
+  return nil if arr.empty?
+
+  min_value = arr[0]
+  left_index = 0
+  right_index = arr.length - 1
+
+  while left_index <= right_index
+    mid_index = (left_index + right_index) / 2
+    mid_value = arr[mid_index]
+
+    if mid_value < min_value
+      min_value = mid_value
+    end
+
+    if mid_value < arr[right_index]
+      right_index = mid_index - 1
+    else
+      left_index = mid_index + 1
+    end
+  end
+
+  min_value
+end
+
+p find_lowest_integer(arr)
 
 
 # Simple class example
