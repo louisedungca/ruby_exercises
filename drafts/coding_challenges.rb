c73f2177f046d09558f77e0b3b7aca410fd7e11f
Louise Dungca <dungca.louisechristine@gmail.com>
2024-02-05 16:03:34 +0800

    add vowel code challenge

--- drafts/coding_challenges.rb
+++ drafts/coding_challenges.rb
@@ -52,7 +52,7 @@ def remove_vowels(str)
   final_str = ""
 
   str.each_char do |char|
-    if !vowels_arr.include?(char)
+    if vowels_arr.none? { |vowel| vowel == char }
       final_str += char
     end
   end
@@ -119,3 +119,23 @@ list(data)
 ## the vowel code
 # Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern: a -> 1, e -> 2, i -> 3, o -> 4, u -> 5
 # Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
+VOWELS = {
+  "a" => 1,
+  "e" => 2,
+  "i" => 3,
+  "o" => 4,
+  "u" => 5,
+}
+
+def encode(str)
+  encoded_str = str.downcase.chars.map { |char| VOWELS[char] || char }.join
+  p encoded_str
+end
+
+def decode(encoded_str)
+  decoded_str = encoded_str.downcase.chars.map { |char| VOWELS.key(char.to_i) || char }.join
+  p decoded_str
+end
+
+encoded_str = encode("hello")
+decode(encoded_str)
